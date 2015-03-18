fed348f7f34af5a0122d20c5d16bbf7a9f628790
Evgeniy J <jemguns@gmail.com>
2015-03-19 02:56:42 +0400

    first commit

---
+++ scripts/fake-data.js
@@ -0,0 +1,93 @@
+﻿var products = [{
+    id:1234,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов","Москва","Питер"]
+},
+{
+    id: 1236,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+},
+{
+    id: 1238,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+},
+{
+    id: 144,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+},
+{
+    id: 12444,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+},
+{
+    id: 13234,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+},
+{
+    id: 22334,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+},
+{
+    id: 3734,
+    name: "Product1",
+    email: "narkotiki@mail.ru",
+    count: 5,
+    price: 42345,
+    delivery: ["Страна", "Город"],
+    country: "Россия",
+    city: ["Саратов", "Москва", "Питер"]
+}];
+
+var delivery = [{
+    country: "Россия",
+    cities: ["Саратов", "Питер", "Москва"]
+},
+{
+    country: "США",
+    cities: ["LA", "NY", "Springfield"]
+},
+{
+    country: "Япония",
+    cities: ["Токио", "Акита", "Тояма"]
+}];
---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="jquery-2.1.3.js" />
+
+
+//$(document).ready(function () { // вся магия после загрузки страницы
+//        
+//});
+var close_modal = function () {
+    $('#modal_form').css('display', 'none') // убираем у модального окна display: none;
+					.animate({ opacity: 0, top: '50%' }, 200);
+    $('#overlay').fadeOut(400);
+};
+
+
+
+//$(document).on('click', '.name a', function () {
+//    $('#overlay').fadeIn(400, // сначала плавно показываем темную подложку
+//		 	function(){ // после выполнения предъидущей анимации
+//		 	    $('#modal_form') 
+//					.css('display', 'block') // убираем у модального окна display: none;
+//					.animate({opacity: 1, top: '50%'}, 200); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+//		 	});
+//});
+
+
+$(document).on('click', '.close', close_modal);
+
+
